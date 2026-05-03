1ea53513ab44b16baf6e9879830e69d544000978
ShiHou-Sun <eduarda.ferreirah18@gmail.com>
2026-05-03 05:23:43 -0400

    Refactor irProPedido function and remove others

--- script.js
+++ script.js
@@ -2,14 +2,3 @@
 function irProPedido() {
   window.location.href = "pedido.html";
 }
-
-function aceitou() {
-    alert("EU SOU CASADOOOO, YEAAAH!!");
-}
-
-function foge() {
-    let btn = document.getElementById("no");
-    let x = Math.random() * 200 - 100;
-    let y = Math.random() * 200 - 100;
-    btn.style.transform = `translate(${x}px, ${y}px)`;
-}
